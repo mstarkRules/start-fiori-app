ffc281d4da62a42b2f1a283e1c98944261d4fdb6
Marcos Paulo Amorim <markstm10@gmail.com>
2023-08-06 15:56:32 -0300

    feat: (setup project template) sets initials

---
+++ webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["startfioriapp/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"start-fiori-app/test/unit/controller/View1.controller"
+], function () {
+	"use strict";
+});
